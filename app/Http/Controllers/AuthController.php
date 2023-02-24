2c208afbf84e0dd7e49dccb3abbe4fd605a4b83d
Vitor <vitorbizarracontato@gmail.com>
2023-02-24 15:45:36 -0300

    sanctum auth api endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!auth()->attempt($credentials)) {
+            abort(401, 'Invalid Credentials.');
+        }
+
+        $token = auth('sanctum')->user()->createToken('access_token');
+
+        return response()->json(['data' => ['token' => $token->plainTextToken]]);
+    }
+
+    public function register(Request $request, User $user)
+    {
+        $userData = $request->only('name', 'email', 'password');
+        $userData['password'] = bcrypt($userData['password']);
+
+        if (!$user = $user->create($userData)) {
+            abort(500, 'Error to create a new user.');
+        }
+
+
+        return response()->json(['data' => ['user' => $user]]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('v1')->group(function(){
+    Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+    
+    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
 
+    Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
